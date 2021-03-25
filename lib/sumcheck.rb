2df759d79dd98e02d60409e178d45bb3b9f272fc
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-03-25 11:25:43 +0000

    Refactored calculations into private method

--- lib/sumcheck.rb
+++ lib/sumcheck.rb
@@ -2,11 +2,17 @@ class Sumcheck
   def scan(sum)
     sum_array = sum.split(" ")
     array = []
+    x = sum_evaluation(sum_array)
+    array += [sum, x]
+  end
+
+  private
+
+  def sum_evaluation(sum_array)
     x = 0
     sum_array.each do |num|
       x += num.to_i
     end
-    array << sum
-    array << x
+    return x
   end
 end

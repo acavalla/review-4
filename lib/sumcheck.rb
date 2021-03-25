6a8ab048da819c996e1e9052921a3af4f6389d55
Anna Cavalla <Anna.Cavalla@gmail.com>
2021-03-25 11:38:54 +0000

    Refactored

--- lib/sumcheck.rb
+++ lib/sumcheck.rb
@@ -2,23 +2,33 @@ class Sumcheck
   def scan(sum)
     sum_array = sum.split(" ")
     array = []
-    x = sum_evaluation(sum_array)
+    x = sum_filter(sum_array)
     array += [sum, x]
   end
 
   private
 
-  def sum_evaluation(sum_array)
-    x = 0
+  def sum_filter(sum_array)
     if sum_array[1] == "+"
-      sum_array.each do |num|
-        x += num.to_i
-      end
+      x = add(sum_array)
     else
-      sum_array.each do |num|
-        x = 1
-        x = num.to_i
-      end
+      x = multiply(sum_array)
+    end
+    return x
+  end
+
+  def add(sum_array)
+    x = 0
+    sum_array.each do |num|
+      x += num.to_i
+    end
+    return x
+  end
+
+  def multiply(sum_array)
+    x = 1
+    sum_array.each do |num|
+      x = num.to_i
     end
     return x
   end
